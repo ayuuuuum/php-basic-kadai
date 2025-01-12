ec29845390aeb60a98edb4569bebe5513bef3d40
ayumi <jackie19980824@gmail.com>
2025-01-13 08:44:35 +0900

    kadai_005を再度アップロード
    「テストの平均点を計算しよう」を修正しました。

--- kadai_005/score.php
+++ kadai_005/score.php
@@ -18,7 +18,9 @@
             $score9 = 30;
             $score10 = 60;
 
-            echo ($score1 + $score2 + $score3 + $score4 + $score5 + $score6 + $score7 + $score8 + $score9 + $score10) / 10;
+            $total_score = $score1 + $score2 + $score3 + $score4 + $score5 + $score6 + $score7 + $score8 + $score9 + $score10;
+
+            echo $total_score / 10;
             ?>
         </p>
     </body>
