b13f02e6276888cc43e21eb01697242ec3e7f755
ayumi <jackie19980824@gmail.com>
2025-01-20 13:35:59 +0900

    課題再提出

--- kadai_sort/sort.php
+++ kadai_sort/sort.php
@@ -9,32 +9,44 @@
 <body>
     <p>
         <?php
-        function sort_2way($nums, $orderBy = 'asc') {
-            if ($orderBy === 'asc') {
-                sort($nums);
+        /*
+        関数名	sort_2way	
+        引数	$array：ソート対象の配列  $order：ソート方向 （TRUE：昇順／FALSE：降順）
+        戻り値	　なし
+        機能　　第2引数に指定したソート方向（昇順／降順）で、第1引数の配列をソートする。　*/
+        function sort_2way($array, $order) {
+            //引数$oderがture(昇順)か？
+            if ($order ) {
+                //メッセージを表示　昇順にソートします。
                 echo '昇順にソートします。<br>';
-            } elseif ($orderBy === 'desc') {
-                rsort($nums);
-                echo '降順にソートします。<br>';
+                //引数$arrayを昇順にソート
+                sort($array);
             } else {
-                echo '無効なソート順が指定されました。<br>';
-                return $nums;
+                //メッセージを表示　降順にソートします。
+                echo '降順にソートします。<br>';
+                //引数$arrayを降順にソート
+                rsort($array);
             }
             
             // ソートされた配列を表示
-            foreach ($nums as $num) {
+            foreach ($array as $num) {
+                //配列の中身を一行ずつ表示
                 echo "{$num}<br>";
             }
-        
-            return $nums; // ソート後の配列を返す
         }
         
-        // 使用例
+        // ソートする配列を宣言
         $nums = [15, 4, 18, 23, 10 ];
-        sort_2way($nums, 'asc');
-        sort_2way($nums, 'desc');
+        //昇順にソートする
+        sort_2way($nums, TRUE);
+        //降順にソートする
+        sort_2way($nums, FALSE);
         ?>
+
     </p>
 </body>
 
 </html>
+
+
+
